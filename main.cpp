d79946027a837e222d04add7cf9670d2a5b7b88c
Neçirvan Alpar <104355823+necirvanalpar@users.noreply.github.com>
2025-03-31 01:47:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 ﻿// Dear ImGui: standalone example application for DirectX 11
 
-// Learn about Dear ImGui:
+// Learn about Dear ImGui
 // - FAQ                  https://dearimgui.com/faq
 // - Getting Started      https://dearimgui.com/getting-started
 // - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
